5ad4eb521e9c0bf36b3342e2fdafd2698ce312a3
Aniket-garg04 <aniketgarg915@gmail.com>
2025-08-30 14:12:46 +0530

    Update LS.sh

--- scripts/LS.sh
+++ scripts/LS.sh
@@ -1,44 +1,49 @@
-# LS + 0
-python main.py --asr facebook/wav2vec2-base-960h \
-                --steps 10 \
-                --dataset_name librispeech \
-                --dataset_dir /home/daniel094144/data/LibriSpeech \
-                --temp 2.5 \
-                --episodic \
-                --em_coef 0.3 \
-                --reweight \
-                --log_dir exps \
-                --lr 2e-5 \
-                --non_blank \
-                --train_feature \
-                --extra_noise 0 \
+#!/bin/bash
 
-# LS + 0.005
-python main.py --asr facebook/wav2vec2-base-960h \
-                --steps 10 \
-                --dataset_name librispeech \
-                --dataset_dir /home/daniel094144/data/LibriSpeech \
-                --temp 2.5 \
-                --episodic \
-                --em_coef 0.3 \
-                --reweight \
-                --log_dir exps \
-                --lr 2e-5 \
-                --non_blank \
-                --train_feature \
-                --extra_noise 0.005 \
+# === LibriSpeech + 0 (no noise) ===
+python main.py \
+  --asr facebook/wav2vec2-base-960h \
+  --steps 10 \
+  --dataset_name librispeech \
+  --dataset_dir ./LibriSpeech \
+  --temp 2.5 \
+  --episodic \
+  --em_coef 0.3 \
+  --reweight \
+  --log_dir exps/noise_0 \
+  --lr 2e-5 \
+  --non_blank \
+  --train_feature \
+  --extra_noise 0
 
-# LS + 0.01
-python main.py --asr facebook/wav2vec2-base-960h \
-                --steps 10 \
-                --dataset_name librispeech \
-                --dataset_dir /home/daniel094144/data/LibriSpeech \
-                --temp 2.5 \
-                --episodic \
-                --em_coef 0.3 \
-                --reweight \
-                --log_dir exps \
-                --lr 2e-5 \
-                --non_blank \
-                --train_feature \
-                --extra_noise 0.01 \
+# === LibriSpeech + 0.005 noise ===
+python main.py \
+  --asr facebook/wav2vec2-base-960h \
+  --steps 10 \
+  --dataset_name librispeech \
+  --dataset_dir ./LibriSpeech \
+  --temp 2.5 \
+  --episodic \
+  --em_coef 0.3 \
+  --reweight \
+  --log_dir exps/noise_0.005 \
+  --lr 2e-5 \
+  --non_blank \
+  --train_feature \
+  --extra_noise 0.005
+
+# === LibriSpeech + 0.01 noise ===
+python main.py \
+  --asr facebook/wav2vec2-base-960h \
+  --steps 10 \
+  --dataset_name librispeech \
+  --dataset_dir ./LibriSpeech \
+  --temp 2.5 \
+  --episodic \
+  --em_coef 0.3 \
+  --reweight \
+  --log_dir exps/noise_0.01 \
+  --lr 2e-5 \
+  --non_blank \
+  --train_feature \
+  --extra_noise 0.01
